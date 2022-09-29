bc46efc78ff2bcdbce9032b650f3fec2bb16b7c8
m4tiolli <97938132+m4tiolli@users.noreply.github.com>
2022-09-28 22:10:25 -0300

    css melhorado

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,72 +1,84 @@
-import React, {useState} from "react";
-import {View, Text, TextInput, TouchableOpacity, StyleSheet} from 'react-native';
+import React, { useState } from "react";
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 
-export default function Home(){
+export default function Home() {
     let [dolar, setDolar] = useState();
     let [cotacao, setCotacao] = useState();
     let [total, setTotal] = useState();
 
-    function ConverterReal(){
-        let real =parseFloat(dolar) * parseFloat(cotacao);    
+    function ConverterReal() {
+        let real = parseFloat(dolar) * parseFloat(cotacao);
         setTotal(real);
     }
-    return(
-        <View style={styles.container}> 
-            <Text style={styles.title}>Conversor para Real</Text>
-            <TextInput style={styles.input} onChangeText={setDolar} placeholder="Valor em Dolar"></TextInput>
-            <TextInput style={styles.input} onChangeText={setCotacao} placeholder="Cotação de Hoje">
 
-            </TextInput>
-              <Text style={styles.subtitulo}>US$ {dolar} é igual a: RS$ {total}</Text>
-            <TouchableOpacity style={styles.button} onPress={ConverterReal}>
-                <Text style={styles.buttonText}>Converter</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity style={styles.button}>
-                <Text style={styles.buttonText}>Limpar</Text>
-            </TouchableOpacity>
+    return (
+        <View style={styles.container}>
+            <View style={styles.cont}>
+                <Text style={styles.title}>Conversor para Real</Text>
+                <TextInput style={styles.input} onChangeText={setDolar} placeholder="Valor em Dolar"></TextInput>
+                <TextInput style={styles.input} onChangeText={setCotacao} placeholder="Cotação atual"></TextInput>
+                <TouchableOpacity style={styles.button} onPress={ConverterReal}>
+                    <Text style={styles.buttonText}>Converter</Text>
+                </TouchableOpacity>
+                <Text style={styles.subtitulo}>US$ {dolar} é igual a: R$ {total}</Text>
+            </View>
         </View>
     )
 }
 const styles = StyleSheet.create({
-    container:{
-        backgroundColor: '#D6FFB3',
+    container: {
+        backgroundColor: "#EFEFEF",
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
     },
-    title:{
+    cont: {
+        width: 500,
+        height: 650,
+        backgroundColor: "#FFFFFF",
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 25,
+        shadowColor: "#000000",
+        shadowOpacity: 0.3,
+        shadowRadius: 20,
+        shadowOffset: {
+            height: 5,
+            width: 5
+        }
+    },
+    title: {
         fontSize: 35,
         color: 'black',
-        fontWeight: 'bold'
+        fontWeight: 300,
     },
-    subtitulo:{
+    subtitulo: {
         color: '#000',
         fontSize: 20,
-        fontWeight: 'bold',
-        marginTop: 15
+        marginTop: 75
     },
-    input:{
-        backgroundColor: 'white',
+    input: {
+        backgroundColor: '#efefef',
         color: 'black',
-        marginTop: 30,
-        padding: 15,
-        borderRadius: 10,
-        textAlign: 'center',
-        width: 150
-    },
-    button:{
-        backgroundColor: '#13FF00',
-        color: 'white',
-        marginTop: 30,
+        marginTop: 40,
         padding: 10,
-        borderRadius: 10,
+        borderRadius: 50,
+        textAlign: 'center',
+        width: 300,
+        height: 40
+    },
+    button: {
+        backgroundColor: '#efefef',
+        color: 'white',
+        marginTop: 100,
+        padding: 10,
+        borderRadius: 50,
         width: 200,
     },
-    buttonText:{
-        fontSize:20,
+    buttonText: {
+        fontSize: 20,
         textAlign: 'center',
         textTransform: 'uppercase',
-        fontWeight: 700
+        fontWeight: 300
     }
 })
